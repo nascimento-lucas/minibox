2bb185483342a91f7a6bec05a2742901dd705fea
@lucasnascimento <lucas.nascimento.silva@ccc.ufcg.edu.br>
2025-07-31 16:17:18 -0300

    update no front

--- trindasbox/src/app/cadastro/cadastro.ts
+++ trindasbox/src/app/cadastro/cadastro.ts
@@ -7,7 +7,6 @@ import { BackendService } from '../backend-service';
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.css',
 })
-
 export class Cadastro {
   constructor(private backendService: BackendService) {}
   tipoSelecionado = '';
@@ -16,7 +15,7 @@ export class Cadastro {
     nome: '',
     numeroCracha: '',
     valor: '',
-  }
+  };
   produto = {
     tipo: 'artigoReligioso',
     nome: '',
@@ -24,92 +23,72 @@ export class Cadastro {
     id: '',
     quantidade: '',
     valor: '',
-  }
+  };
   alimento = {
     tipo: 'alimento',
     nome: '',
     quantidade: '',
     valor: '',
-    id:'',
-  }
+    id: '',
+  };
 
   confirmar() {
-    if (this.tipoSelecionado === "") {
-      alert("Por favor, selecione um tipo.");
+    if (this.tipoSelecionado === '') {
+      alert('Por favor, selecione um tipo.');
       return;
-    }
-    else if (this.tipoSelecionado === 'usuarios') {
-      if(!this.usuario.numeroCracha) {
-        alert("Por favor, preencha o número do crachá.");
+    } else if (this.tipoSelecionado === 'usuarios') {
+      if (!this.usuario.numeroCracha) {
+        alert('Por favor, preencha o número do crachá.');
         return;
       }
       this.backendService.criarUsuario(this.usuario).subscribe((res: any) => {
         console.log(res);
-        alert("Cadastro realizado com sucesso!");
+        alert('Cadastro realizado com sucesso!');
       });
-    }
-    else if (this.tipoSelecionado === 'religiosos') {
-      if(!this.produto.nome || !this.produto.descricao || !this.produto.id || !this.produto.quantidade || !this.produto.valor) {
-        alert("Por favor, preencha todos os campos.");
+    } else if (this.tipoSelecionado === 'religiosos') {
+      if (
+        !this.produto.nome ||
+        !this.produto.descricao ||
+        !this.produto.id ||
+        !this.produto.quantidade ||
+        !this.produto.valor
+      ) {
+        alert('Por favor, preencha todos os campos.');
         return;
       }
       this.backendService.criarProduto(this.produto).subscribe((res: any) => {
         console.log(res);
-        alert("Cadastro realizado com sucesso!");
+        alert('Cadastro realizado com sucesso!');
       });
-    }
-    else if (this.tipoSelecionado === 'alimentos') {
-      if(!this.alimento.nome || !this.alimento.quantidade || !this.alimento.valor) {
-        alert("Por favor, preencha todos os campos.");
+    } else if (this.tipoSelecionado === 'alimentos') {
+      if (
+        !this.alimento.nome ||
+        !this.alimento.quantidade ||
+        !this.alimento.valor
+      ) {
+        alert('Por favor, preencha todos os campos.');
         return;
       }
       this.backendService.criarAlimento(this.alimento).subscribe((res: any) => {
         console.log(res);
-        alert("Cadastro realizado com sucesso!");
+        alert('Cadastro realizado com sucesso!');
       });
     }
     this.usuario = { tipo: 'usuario', nome: '', numeroCracha: '', valor: '' };
-    this.produto = { tipo: 'artigoReligioso', nome: '', descricao: '', id: '', quantidade: '', valor: '' };
-    this.alimento = { tipo: 'alimento', nome: '', quantidade: '', valor: '', id:'' };
+    this.produto = {
+      tipo: 'artigoReligioso',
+      nome: '',
+      descricao: '',
+      id: '',
+      quantidade: '',
+      valor: '',
+    };
+    this.alimento = {
+      tipo: 'alimento',
+      nome: '',
+      quantidade: '',
+      valor: '',
+      id: '',
+    };
   }
-
-  // setValue(value: string) {
-  //   switch (value) {
-  //     case 'nomeAlimento':
-  //       this.alimento.nome = value;
-  //       break;
-  //     case 'idAlimento':
-  //       this.alimento.id = value;
-  //       break;
-  //     case 'qtd':
-  //       this.alimento.quantidade = value;
-  //       break;
-  //     case 'valorAlimento':
-  //       this.alimento.valor = value;
-  //       break;
-  //     case 'nomeProduto':
-  //       this.produto.nome = value;
-  //       break;
-  //     case 'descricao':
-  //       this.produto.descricao = value;
-  //       break;
-  //     case 'idProduto':
-  //       this.produto.id = value;
-  //       break;
-  //     case 'qtd':
-  //       this.produto.quantidade = value;
-  //       break;
-  //     case 'valorProduto':
-  //       this.produto.valor = value;
-  //       break;
-  //     case 'nomeUsuario':
-  //       this.usuario.nome = value;
-  //       break;
-  //     case 'numeroCracha':
-  //       this.usuario.numeroCracha = value;
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  // }
 }
--- trindasbox/src/app/consulta/consulta.ts
+++ trindasbox/src/app/consulta/consulta.ts
@@ -19,45 +19,62 @@ export class Consulta {
   consultarTodos() {
     switch (this.tipoConsultaSelecionado) {
       case 'usuarios':
-        this.backend.listarUsuariosEmDivida().subscribe(res => {this.resultado = res
+        this.backend.listarUsuariosEmDivida().subscribe((res) => {
+          this.resultado = res;
           console.log(res);
         });
         break;
       case 'alimentos':
-        this.backend.listarAlimentos().subscribe(res => {this.resultado = res
-          console.log(res);
-        });;
-        break;
-      case 'produtos':
-        this.backend.listarProdutos().subscribe(res => {this.resultado = res
+        this.backend.listarAlimentos().subscribe((res) => {
+          this.resultado = res;
           console.log(res);
         });
         break;
-      }
+      case 'produtos':
+        this.backend.listarProdutos().subscribe((res) => {
+          this.resultado = res;
+          console.log(res);
+        });
+        break;
     }
-  
+  }
+
   buscarProdutoPorId() {
     if (this.tipoConsultaSelecionado) {
-      this.backend.buscarProdutoPorId(this.idProduto).subscribe(res => {this.resultado = res
+      this.backend.buscarProdutoPorId(this.idProduto).subscribe((res) => {
+        this.resultado = res;
         console.log(res);
       });
     }
-  
   }
 
   buscarUsuarioPorCracha() {
     if (this.numeroCracha) {
-      this.backend.buscarUsuario(this.numeroCracha).subscribe(res => {this.resultado = res
+      this.backend.buscarUsuario(this.numeroCracha).subscribe((res) => {
+        this.resultado = res;
         console.log(res);
-      });;
+      });
     }
   }
 
   buscarAlimentoPorId() {
     if (this.idAlimento) {
-      this.backend.buscarAlimentoPorId(this.idAlimento).subscribe(res => {this.resultado = res
+      this.backend.buscarAlimentoPorId(this.idAlimento).subscribe((res) => {
+        this.resultado = res;
         console.log(res);
-      });;
+      });
     }
+  }
+
+  isArray(obj: any): boolean {
+    return Array.isArray(obj);
+  }
+
+  isObject(obj: any): boolean {
+    return obj && typeof obj === 'object' && !Array.isArray(obj);
+  }
+
+  getKeys(obj: any): string[] {
+    return Object.keys(obj);
   }
 }
